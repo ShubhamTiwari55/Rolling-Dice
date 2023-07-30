709dea36c1cde245d2903e610a9655ebab95766a
ShubhamTiwari55 <stofficial059@gmail.com>
2023-07-30 16:19:22 +0530

    animation added

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -6,22 +6,57 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin{
   int leftDiceNum = 1;
   int rightDiceNum = 1;
+  late AnimationController _controller;
+  late CurvedAnimation animation;
+
   @override
+  void initState() {
+    super.initState();
+    animate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  animate(){
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+        animation = CurvedAnimation(parent: _controller,curve: Curves.bounceOut);
+        animation.addListener(() {
+          setState(() {
+          });
+        });
+        animation.addStatusListener((status) {
+          if(status == AnimationStatus.completed){
+              setState(() {
+                leftDiceNum = Random().nextInt(6)+1;
+                rightDiceNum = Random().nextInt(6)+1;
+            });
+              _controller.reverse();
+          }
+        });
+
+  }
+
   void roll(){
-    setState(() {
-      leftDiceNum = Random().nextInt(6)+1;
-      rightDiceNum = Random().nextInt(6)+1;
-    });
+    _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dice App'),
+        backgroundColor: Colors.black,
+        title: Container(
+            child: Text('Dice App', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),)),
+
       ),
       body: Center(
         child: Column(
@@ -30,32 +65,35 @@ class _HomeScreenState extends State<HomeScreen>{
             Row(
               children: [
                 Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage('assets/images/dice-$leftDiceNum.png'),
+                  child: GestureDetector( onDoubleTap: roll,
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Image(height: 200 - (animation.value)*200,
+                        image: AssetImage('assets/images/dice-$leftDiceNum.png',),
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.all(15),
-                    child: Image(
-                      image: AssetImage(
-                          'assets/images/dice-$rightDiceNum.png'),
+                  child: GestureDetector( onDoubleTap: roll,
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Image(height: 200 - (animation.value)*200,
+                        image: AssetImage(
+                            'assets/images/dice-$rightDiceNum.png'),
+                      ),
                     ),
                   ),
                 )
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                onPressed: roll,
-                child: Text(
-                  'Roll',
-                  style: TextStyle(fontWeight: FontWeight.bold,),
-                ),
+            SizedBox(height: 50,),
+            ElevatedButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.black, fixedSize: const Size(250, 50)),
+              onPressed: roll,
+              child: Text(
+                'Roll the dice',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0,),
               ),
             )
           ],
